a001a5ff97b2ec884a2949450793feb723b28ff5
vitor00981 <eo01018@gmail.com>
2025-04-25 23:14:17 -0500

    Add files via upload

---
+++ Descrição do site.md
@@ -0,0 +1,9 @@
+## Site de games com base em análises bem-humoradas. Como será? ##
+
+A proposta é de uma montagem de um site com base em experiências de jogos que tinha jogado, a maioria no universo Mario, marca registrada da Nintendo. Desde jogos originais até hackroms, eles marcaram minha vida porque era um dos meus tipos favoritos de entretenimento. Será usado o .js (JavaScript) no primeiro momento.
+
+## Como posso ajudar? 
+
+Você pode realizar issues e pull requests por aqui. Sem problemas ou custos. É de graça! O que quiser fazer para melhorar é bem-vindo! 
+
+
---
+++ lion-games.js
@@ -0,0 +1,19 @@
+const myHeading = document.querySelector("h2");
+myHeading.textContent = "Lion-X Gaming Article Project";
+
+myHeading.textContent = "Bem-vindos ao site!";
+let data = [
+  {
+    nome: "Super Mario World",
+    ano: 1990 - 1991
+    descrição: É um jogo eletrônico produzido pela produtora japonesa Nintendo entre os anos de 1990 e 1991.,
+    Fez tanto sucesso mundo afora que o videogame ganhou a atenção de inúmeros editores com o Lunar Magic, programa para Windows,
+    que dá ao jogador inúmeras possibilidades de criar histórias com o encanador italiano ou até outro personagem de sua preferência.,
+    Há outros programas de edição específicos. Basicamente o personagem, junto com o Luigi e o Yoshi, vão juntos atrás de uma aventura,
+    pelos sete mundos. Isso todo mundo já sabe de cor e salteado. Uma das hackroms mais conhecidas na história é o Brutal Mario World (ou Super Kitiku Mario),
+    que reúne inúmeros universos e personagens num só jogo. Kirby, Final Fantasy, entre outros estão marcando presença.,
+    Seu sucessor, Yoshi's Island, jogo lançado em 1995, a história é diferente, pois o Baby Mario,
+    está montado no Yoshi, para resgatar Luigi das mãos de Baby Bowser.
+
+  },
+]
